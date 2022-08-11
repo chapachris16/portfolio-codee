564833c68d2f58db7d4cee6e51437541c2e7dfce
christopher chapa <chapachristopher16@gmail.com>
2022-08-10 22:25:20 -0500

    almost finished bare min

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -4,9 +4,10 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['l','o','b','o','d','a','n']
+    const nameArray = [' ','C','h','r','i','s']
     const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
 
     useEffect(() => {
@@ -16,6 +17,7 @@ const Home = () => {
     },[])
 
   return (
+    <>
     <div className="container home-page">
         <div className="text-zone">
             <h1>
@@ -24,7 +26,7 @@ const Home = () => {
             <br/>
             <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
-            <img src={LogoTitle} alt='developer'/>
+            {/* <img src={LogoTitle} alt='developer'/> */}
             <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
             <br/>
             <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
@@ -34,6 +36,8 @@ const Home = () => {
         </div>
       <Logo/>
     </div>
+    <Loader type='pacman'/>
+    </>
   )
 }
 
